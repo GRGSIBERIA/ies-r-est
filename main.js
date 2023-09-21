c6d54ba1374df3d06653312b2ba5272bf1d47fee
Eiichi Takebuchi <nanashi4129@gmail.com>
2023-09-21 15:34:28 +0900

    出来事を評価する部分を追加

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 question = [
-    {"id":  1, "factor": 0,"text": "どんなきっかけでも，そのことを思い出すと，そのときの気もちがぶりかえしてくる。"},
-    {"id":  2, "factor": 0,"text": "睡眠の途中で目がさめてしまう。"},
-    {"id":  3, "factor": 0,"text": "別のことをしていても，そのことが頭から離れない。"},
-    {"id":  4, "factor": 2,"text": "イライラして，怒りっぽくなっている。"},
-    {"id":  5, "factor": 1,"text": "そのことについて考えたり思い出すときは，なんとか気を落ちつかせるようにしている。"},
-    {"id":  6, "factor": 0,"text": "考えるつもりはないのに，そのことを考えてしまうことがある。"},
-    {"id":  7, "factor": 1,"text": "そのことは，実際には起きなかったとか，現実のことではなかったような気がする。"},
-    {"id":  8, "factor": 1,"text": "そのことを思い出させるものには近よらない。"},
-    {"id":  9, "factor": 0,"text": "そのときの場面が，いきなり頭にうかんでくる。"},
-    {"id": 10, "factor": 2,"text": "神経が敏感になっていて，ちょっとしたことでどきっとしてしまう。"},
-    {"id": 11, "factor": 1,"text": "そのことは考えないようにしている。"},
-    {"id": 12, "factor": 1,"text": "そのことについては，まだいろいろな気もちがあるが，それには触れないようにしている。"},
-    {"id": 13, "factor": 1,"text": "そのことについての感情は，マヒしたようである。"},
-    {"id": 14, "factor": 0,"text": "気がつくと，まるでそのときにもどってしまったかのように，ふるまったり感じたりすることがある。"},
-    {"id": 15, "factor": 2,"text": "寝つきが悪い。"},
-    {"id": 16, "factor": 0,"text": "そのことについて，感情が強くこみあげてくることがある。"},
-    {"id": 17, "factor": 1,"text": "そのことを何とか忘れようとしている。"},
-    {"id": 18, "factor": 2,"text": "ものごとに集中できない。"},
-    {"id": 19, "factor": 2,"text": "そのことを思い出すと，身体が反応して，汗ばんだり，息苦しくなったり，むかむかしたり，どきどきすることがある。"},
-    {"id": 20, "factor": 0,"text": "そのことについての夢を見る。"},
-    {"id": 21, "factor": 2,"text": "警戒して用心深くなっている気がする。"},
-    {"id": 22, "factor": 1,"text": "そのことについては話さないようにしている。"}
+    {"ord": 0, "id":  1, "factor": 0,"text": "どんなきっかけでも，そのことを思い出すと，そのときの気もちがぶりかえしてくる。"},
+    {"ord": 0, "id":  2, "factor": 0,"text": "睡眠の途中で目がさめてしまう。"},
+    {"ord": 0, "id":  3, "factor": 0,"text": "別のことをしていても，そのことが頭から離れない。"},
+    {"ord": 0, "id":  4, "factor": 2,"text": "イライラして，怒りっぽくなっている。"},
+    {"ord": 0, "id":  5, "factor": 1,"text": "そのことについて考えたり思い出すときは，なんとか気を落ちつかせるようにしている。"},
+    {"ord": 0, "id":  6, "factor": 0,"text": "考えるつもりはないのに，そのことを考えてしまうことがある。"},
+    {"ord": 0, "id":  7, "factor": 1,"text": "そのことは，実際には起きなかったとか，現実のことではなかったような気がする。"},
+    {"ord": 0, "id":  8, "factor": 1,"text": "そのことを思い出させるものには近よらない。"},
+    {"ord": 0, "id":  9, "factor": 0,"text": "そのときの場面が，いきなり頭にうかんでくる。"},
+    {"ord": 0, "id": 10, "factor": 2,"text": "神経が敏感になっていて，ちょっとしたことでどきっとしてしまう。"},
+    {"ord": 0, "id": 11, "factor": 1,"text": "そのことは考えないようにしている。"},
+    {"ord": 0, "id": 12, "factor": 1,"text": "そのことについては，まだいろいろな気もちがあるが，それには触れないようにしている。"},
+    {"ord": 0, "id": 13, "factor": 1,"text": "そのことについての感情は，マヒしたようである。"},
+    {"ord": 0, "id": 14, "factor": 0,"text": "気がつくと，まるでそのときにもどってしまったかのように，ふるまったり感じたりすることがある。"},
+    {"ord": 0, "id": 15, "factor": 2,"text": "寝つきが悪い。"},
+    {"ord": 0, "id": 16, "factor": 0,"text": "そのことについて，感情が強くこみあげてくることがある。"},
+    {"ord": 0, "id": 17, "factor": 1,"text": "そのことを何とか忘れようとしている。"},
+    {"ord": 0, "id": 18, "factor": 2,"text": "ものごとに集中できない。"},
+    {"ord": 0, "id": 19, "factor": 2,"text": "そのことを思い出すと，身体が反応して，汗ばんだり，息苦しくなったり，むかむかしたり，どきどきすることがある。"},
+    {"ord": 0, "id": 20, "factor": 0,"text": "そのことについての夢を見る。"},
+    {"ord": 0, "id": 21, "factor": 2,"text": "警戒して用心深くなっている気がする。"},
+    {"ord": 0, "id": 22, "factor": 1,"text": "そのことについては話さないようにしている。"}
 ];
 
 const shuffleArray = (arr) => {
@@ -35,9 +35,6 @@ const shuffleArray = (arr) => {
 };
 
 window.addEventListener("load", (event) => {
-    const factorEn = ["Instrucsion", "Avoidance", "Hyperarousal"];
-    const factorJa = ["侵入症状", "回避症状", "過覚醒症状"];
-    
     let tab = document.getElementById("qs-tab");
 
     const dce = (e) => { 
@@ -75,6 +72,7 @@ window.addEventListener("load", (event) => {
     let shuffles = shuffleArray(question);
     for (let i = 0; i < shuffles.length; i++) {
         const t = shuffles[i]["text"];
+        shuffles[i]["ord"] = i;
 
         let tr = dce("tr");
 
@@ -87,7 +85,7 @@ window.addEventListener("load", (event) => {
         const makeradio = (td, fac) => {
             let b1 = dce("input");
             b1.type = "radio";
-            b1.name = "id-" + shuffles[i]["id"].toString();
+            b1.name = "name-" + shuffles[i]["id"].toString();
             b1.value = fac;
             td.appendChild(b1)
         }
@@ -105,3 +103,47 @@ window.addEventListener("load", (event) => {
     }
 
 });
+
+function calculate() {
+    const factorEn = ["Instrucsion", "Avoidance", "Hyperarousal", "Total"];
+    const factorJa = ["侵入症状", "回避症状", "過覚醒症状", "合計点"];
+    let factors = [0, 0, 0, 0]
+    
+    for (let i = 0; i < question.length-1; ++i) {
+        let point = 0;
+
+        let rads = document.getElementsByName("name-" + question[i]["id"].toString());
+
+        for (let j = 0; j < 5; j++) {
+            
+            if (rads.item(j).checked === true) {
+                point = j;
+                break;
+            }
+        }
+
+        let facid = question[i]["factor"]
+        factors[facid] += point;
+    }
+    factors[3] = factors[0] + factors[1] + factors[2];
+
+    let tab = document.getElementById("rs-tab");
+    while (fsc = tab.firstChild) {
+        tab.removeChild(fsc);
+    }
+
+    const dce = (e) => { 
+        return document.createElement(e);
+    };
+
+    for (let i = 0; i < factors.length; i++) {
+        let tr = dce("tr");
+        let td_name = dce("td");
+        td_name.innerText = factorJa[i];
+        let td_point = dce("td");
+        td_point.innerText = factors[i].toString();
+        tr.appendChild(td_name);
+        tr.appendChild(td_point);
+        tab.appendChild(tr);
+    }
+}
